33be53b04a0a379ca378159922a48282361547e7
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-10 22:17:36 -0500

    Added App
    This is the first version of Clicker Counter that GutHub has ever seen!

---
+++ Assets/Counter.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var CurrentScore : UnityEngine.UI.Text;
+var HighScore : UnityEngine.UI.Text;
+
+
+function Start () {
+	PlayerPrefs.SetInt("CurrentScore", 0);
+}
+
+function Update () {
+	if (Input.GetMouseButtonDown(0)) {
+		PlayerPrefs.SetInt("CurrentScore", PlayerPrefs.GetInt("CurrentScore") + 1);
+	}
+	if (PlayerPrefs.GetInt("CurrentScore") > PlayerPrefs.GetInt("HighScore")) {
+		PlayerPrefs.SetInt("HighScore", PlayerPrefs.GetInt("CurrentScore"));
+	}
+	CurrentScore.text = PlayerPrefs.GetInt("CurrentScore").ToString();
+	HighScore.text = "HighScore: " + PlayerPrefs.GetInt("HighScore").ToString();
+}
